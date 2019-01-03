4b66c6115a31d4b914ea2ffb6110dad3f4fe7418
Wesley Boynton <wes@boynton.io>
2019-01-03 08:51:21 -0500

    Added Okta scripts

---
+++ okta/okta-cleanup.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name       Okta remove annoying browser extension banner
+// @namespace  http://okta.stay.logged.in.please/
+// @version    0.1
+// @description  Gets rid of some annoying crap in the Okta interface
+// @match      https://intersections.okta.com/*
+// @copyright  2018, Wes Boynton
+// This script ain't elegant but it scraped the crap off of the page that I wanted gone.
+// ==/UserScript==
+
+// Removes elements from the page by class
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+// Removes the parent element of all elements of name `className`
+function removeParentElementByClass(className) {
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        var parent = elements[0].parentNode
+        parent.parentNode.removeChild(parent);
+    }
+}
+
+// Do this several times on a small loop to ensure stuff that pops up after page load is tidied up.
+var x = 0;
+var intervalID = setInterval(function () {
+   // Nix the input tooltips in the login page that interfere with the LastPass login selector icon
+   removeElementsByClass("input-tooltip");
+
+   // Nix the banner that indicates you need the Okta plugin/extension
+   removeElementsByClass("banner-container");
+
+   // Nix the plugins/apps that require the plugin/extension.
+   // They'll reappear magically if you install it.
+   removeParentElementByClass("plugin-required");
+
+   // Announce proudly
+   console.log("Tampermonkey Script cleaned up okta UI garbage!");
+   if (++x === 15) {
+       window.clearInterval(intervalID);
+   }
+}, 100);
+
+
+
+
---
+++ okta/okta-timeout-fix.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name       Okta stay logged in.
+// @namespace  http://okta.stay.logged.in.please/
+// @version    0.1
+// @description  Keeps you logged in to your Okta SSO console
+// @match      https://*.okta.com/*
+// @copyright  2012+, You
+// Borrowed from http://tom-gould.co.uk/blog/stay-logged-in-to-okta-sso
+// ==/UserScript==
+var ssologin = function () {
+    var button = document.getElementById('session-timeout-ok');
+    if (button) {
+        var evt = document.createEvent("HTMLEvents");
+        evt.initEvent('click', true, true);
+        button.dispatchEvent(evt);
+    }
+}
+window.setInterval(ssologin, 10000);
